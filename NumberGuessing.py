8897065c02cc8c324a7f4c2173aafd7418142828
JonHelmus <jonathanchelmus@gmail.com>
2020-03-17 08:14:04 -0700

    Update NumberGuessing.py

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -3,3 +3,24 @@ Tech Degree Project 1
 Number Guessing Game
 Created by Jonathan Helmus
 """)
+
+
+#No errors should be in code
+
+#Text Header To Welcome To Game
+
+#Continously prompt until user gets number correct
+
+#Tell User if number is higher of lower
+
+#Show number of attempts made to user once they get the number correct
+
+#Tell user if they guessed outside the range
+
+#Prompt user to play again once the guessed correctly
+
+#show current high score of game
+
+import random
+
+number = random.randint(1, 20)
