51d4d5833bd9096baaeb0d88cf865b3aed648cd2
Jon <jonathan.helmus@student.ctuonline.edu>
2020-03-18 10:17:09 -0700

    Update NumberGuessing.py

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -4,86 +4,80 @@ Number Guessing Game
 Created by Jonathan Helmus
 """)
 
-# No errors should be in code
 
-# Text Header To Welcome To Game
-
-# Continously prompt until user gets number correct
-
-# Tell User if number is higher of lower
-
-# Show number of attempts made to user once they get the number correct
-
-# Tell user if they guessed outside the range
-
-# Prompt user to play again once the guessed correctly
-
-# show current high score of game
 
 import random, time
 
 
 
-#remove before submitting
+#Prompting the user for  their name
 name = raw_input("Please enter your name: ")
 
+
+#Welcome intro to the game
 def thinkingHello():
     # type: () -> object
     print("\nWelcome to the number guessing game {}, where the object is to guess the correct number!".format(name))
     print("*" * 90)
-    print("To begin, I'm going to need to think of a number between 1 and 20")
+    print("\nTo begin, I'm going to need to think of a number between 1 and 20")
     time.sleep(1)
-    print("Still thinking")
+    print("\nStill thinking")
     time.sleep(1)
-    print("...still...")
+    print("\n...still...")
     time.sleep(1)
-    print("...thinking...")
+    print("\n...thinking...")
     time.sleep(1)
-    print("...wait..." + " ...I... ")
+    print("\n...wait..." + "\n...I... ")
     time.sleep(2)
-    print("\nI got my number!\n")
+    print("\nI got my number!")
     print("*" * 20)
-    print("\nNow it's  your turn!\nPlease guess a number between 1 and 20.")
-
-def header():
-    print("Welcome to the number guessing game {}!\nPlease guess a number between 1 and 20. ".format(name))  # type: object
-#>>>>>>> Stashed changes
+    print("\nNow it's  your turn!")
+    print("*" * 20)
+    print("\nPlease guess a number between 1 and 20.")
 
 
-attempts = 0
 
-listOfAttempts = []
 
+#The game!
 def start_game():
     answer = random.randrange(1, 20)
     attempts = 0
     thinkingHello()
 
-
-    listOfAttempts = []
-    print(answer)
-
-
     guess = int()
 
     while guess != answer:
         guess = input("\nPlease let me know your guess: ")
+        print("*" * 30)
+        #ValueError that stops that the game
+        if int(guess) < 1 or int(guess) > 20:
+            raise ValueError("The number you selected is not allowed in this game.\nPlease try another number")\
+        #If user guess the right answer
         if guess == answer:
-            print("Got it!")
-            print("It took you {} attempts to guess the number".format(attempts + 1))
+            print("\nKA-CHING!!\nYou guessed the correct number!")
+            print("*" * 30)
+            #show users how many attempts they made to get the guess correct.
+            print("It took you {} attempts to guess my secret number!".format(attempts + 1))
             continue
+        #if user guesses to low
         elif guess < answer:
-            print("Your guess is too low")
+            print("Your guess is too low :-(")
+            print("*" * 20)
             attempts += 1
             print(attempts)
+            print("*" * 20)
             continue
+        #if user guess too high!
         else:
-            print("Your guess is too high")
+            print("Your guess is too high :-(")
+            print("*" * 20)
             attempts += 1
             print(attempts)
+            print("*" * 20)
             continue
-        return
 
+
+    #Prompting user if they would like to play again
     while True:
         print("\nWould you like to play again?\nPlease type 'yes' or 'no'\n")
         again = raw_input("")
