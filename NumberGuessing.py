9a4891bede212bbdad27bd8a7a53539dab60a342
JonHelmus <jonathanchelmus@gmail.com>
2020-03-18 07:53:23 -0700

    Update NumberGuessing.py

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -4,30 +4,75 @@ Number Guessing Game
 Created by Jonathan Helmus
 """)
 
+# No errors should be in code
 
-#No errors should be in code
+# Text Header To Welcome To Game
 
-#Text Header To Welcome To Game
+# Continously prompt until user gets number correct
 
-#Continously prompt until user gets number correct
+# Tell User if number is higher of lower
 
-#Tell User if number is higher of lower
+# Show number of attempts made to user once they get the number correct
 
-#Show number of attempts made to user once they get the number correct
+# Tell user if they guessed outside the range
 
-#Tell user if they guessed outside the range
+# Prompt user to play again once the guessed correctly
 
-#Prompt user to play again once the guessed correctly
-
-#show current high score of game
+# show current high score of game
 
 import random
 
-#define number range
-number = random.randint(1, 20)
+header = print("Welcome to the number guessing game!\nPlease guess a number between 1 and 20")
 
-#Game  Header
-
+#remove before submitting
 
 
-#prompt user to take number
+attempts = 0
+
+listOfAttempts = []
+
+def start_game():
+    answer = random.randrange(1, 20)
+    attempts = 0
+
+    listOfAttempts = []
+    print(answer)
+
+    guess = int(input("Please enter your guess: "))
+
+    while guess != answer:
+            guess = int(input("Please enter your guess: "))
+            if guess == answer:
+                print("Got it!")
+                print("It took you {} attepmts to guess the number".format(attempts))
+                continue
+            elif guess < answer:
+                print("Your guess is too low")
+                attempts += 1
+                print(attempts)
+                continue
+            else:
+                print("Your guess is too high")
+                attempts += 1
+                print(attempts)
+                continue
+            return
+    while True:
+        print("\nWould you like to play again?")
+        again = input("")
+        if again.startswith('y' or 'Y'):
+            start_game()
+            break
+        elif again.startswith('n' or 'N'):
+            print("Alrighty, see ya later!")
+            break
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # Kick off the program by calling the start_game function.
+    start_game()
